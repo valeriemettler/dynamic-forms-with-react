3578d6a0dade38dd5aca0e5227106f8324bc27e8
Valerie Mettler <valeriemettler@gmail.com>
2020-07-10 16:45:26 -0700

    add form fields data object

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,5 +1,51 @@
 import React from 'react';
 
+const formFieldsData = [ 
+  {
+    "tag": "input",
+    "name": "first_name",
+    "type": "text",
+    "human_label": "First Name"
+  }, {
+    "tag": "input",
+    "name": "last_name",
+    "type": "text",
+    "human_label": "Last Name"
+  }, {
+    "tag": "input",
+    "name": "email",
+    "type": "email",
+    "human_label": "Email Address"
+  }, {
+    "tag": "input",
+    "name": "phone_number",
+    "type": "text",
+    "human_label": "Phone Number"
+  }, {
+    "tag": "input",
+    "name": "job_title",
+    "type": "text",
+    "human_label": "Job Title"
+  }, {
+    "tag": "input",
+    "name": "date_of_birth",
+    "type": "date",
+    "human_label": "Date of Birth"
+  }, {
+    "tag": "input",
+    "name": "parental_consent",
+    "type": "checkbox",
+    "human_label": "Parental Consent",
+    "conditional": {
+      "name": "date_of_birth",
+      "show_if": (value) => {
+        const now = new Date();
+        return value >= new Date(now.getFullYear() - 13, now.getMonth(), now.getDate());
+      }
+    }
+  }
+];
+
 const onSubmitHandler = e => {
   e.preventDefault();
   console.log('Submitted Form Data:');
@@ -8,8 +54,8 @@ const onSubmitHandler = e => {
 const Form = () => {
   return (
   <form onSubmit={onSubmitHandler}>
-  <h1>Create Your Account</h1>
-  <button className="btn" type="submit">Create My Account</button>
+    <h1>Create Your Account</h1>
+    <button className="btn" type="submit">Create My Account</button>
   </form>
   );
 };
